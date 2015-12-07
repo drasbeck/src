b6e01195eb05aed68afe4eb56b15156969281444
drasbeck <max@drasbeck.dk>
2015-12-07 15:25:36 +0100

    clean up in naming conventions
    making stuff a little more generic

--- ofApp.h
+++ ofApp.h
@@ -7,47 +7,48 @@
 
 
 class ofApp : public ofBaseApp {
-
+    
 public:
-	void setup();
-	void update();
-	void draw();
-
-	void exit();
-
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void mouseEntered(int x, int y);
-	void mouseExited(int x, int y);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-
-	//ofxHapPlayer videoFilm;
-	//ofxHapPlayer videoAnim;
-
-	ofVideoPlayer videoFilm;
-	ofVideoPlayer videoAnim;
-
-	float xFilm, yFilm, xAnim, yAnim, homeAlpha, playAlpha;
-	int homeX, homeY, homeRadius;
-
-	float filmOrigAspect, animOrigAspect;
-
-	int filmStill, animStill;
-
-	bool filmPlaying;
-	bool animPlaying;
-
-	bool screenHome;
-
-	void filmStarted();
-	void animStarted();
-	void homePressed();
-
-	ofTrueTypeFont stdFont;
+    void setup();
+    void update();
+    void draw();
+    
+    void exit();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    //ofxHapPlayer one;
+    ofxHapPlayer two;
+    
+    ofVideoPlayer one;
+    //ofVideoPlayer two;
+    
+    float xOne, yOne, xTwo, yTwo, homeAlpha, playAlpha;
+    int homeX, homeY, homeRadius;
+    
+    float oneOrigAspect, twoOrigAspect;
+    
+    int oneStill, twoStill;
+    
+    bool onePlaying;
+    bool twoPlaying;
+    
+    bool screenHome;
+    
+    void oneStarted();
+    void twoStarted();
+    void homePressed();
+    
+    ofTrueTypeFont stdFont;
+    string fpsStr;
 };
